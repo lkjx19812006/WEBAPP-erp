56bfbe1d4a11373cc1009914a33313e43de8f037
zhubd <zhubd@yaocaimaimai.com>
2017-08-23 17:16:59 +0800

    我的报价

--- pages/purchaseOrders/purchase.js
+++ pages/purchaseOrders/purchase.js
@@ -1,9 +1,12 @@
-;(function(window){
+;
+(function(window) {
 	var formData = {
-		pn: 1,//页码
-		pSize: 15,//页容量
-		breedId:'',//药材id
-		breedName:'',//药材名
+		pn: 1, //页码
+		pSize: 15, //页容量
+		breedId: '', //药材id
+		breedName: '', //药材名
+		intentionId: '59802a3ad95e34aac519f926',//意向id
+
 	};
-	window.formData = formData;	
+	window.formData = formData;
 })(window)
