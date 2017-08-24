6e9012e8817ce8dc653279d81775184e2de88a37
yinqian <yinq@yaocaimaimai.com>
2017-08-24 13:14:19 +0800

    -m“修改提交 版本校验 消息推送

--- libs/httpService.js
+++ libs/httpService.js
@@ -3,12 +3,13 @@
  * */
 (function(CryptoJS, window, mui, Vue) {
 	mui.init();
-	var common = {}
-	common.KEY = window.localStorage.KEY || ''
-	common.SID = window.localStorage.SID || ''
-	common.commonUrl = 'http://192.168.1.142/front'
-	common.version = 1
-	common.difTime = window.localStorage.difTime || 0
+	var common = {};
+	common.H5Share = 'http://192.168.1.142:81/htm5/#/needDetails/'; //'http://apps.yaocaimaimai.com/htm5/#/needDetails/'
+	common.KEY = window.localStorage.KEY || '';
+	common.SID = window.localStorage.SID || '';
+	common.commonUrl = 'http://192.168.1.142/front';
+	common.version = 1;
+	common.difTime = window.localStorage.difTime || 0;
 	common.apiUrl = {
 		login: '/account/erpLogin.do',
 		code_login: '/account/verifiLogin.do',
--- pages/purchaseOrders/purchase.js
+++ pages/purchaseOrders/purchase.js
@@ -5,7 +5,7 @@
 		pSize: 15, //页容量
 		breedId: '', //药材id
 		breedName: '', //药材名
-		intentionId: '59802a3ad95e34aac519f926',//意向id
+		intentionId: '',//意向id
 
 	};
 	window.formData = formData;
