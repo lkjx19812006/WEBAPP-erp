b72e1e9829cac0757771e354300c63c88f92bc64
yinqian <yinq@yaocaimaimai.com>
2017-08-26 12:58:41 +0800

    “修改BUG

--- libs/vueComponents.js
+++ libs/vueComponents.js
@@ -113,7 +113,7 @@
 				this.nowSelectData = params;
 				this.$refs.inputLabel.value = params.label;
 				this.$refs.input.value = params.value;
-				
+
 				//双向绑定
 				this.$emit('input', params.value);
 				this.change();
@@ -131,6 +131,32 @@
 				setTimeout(function() {
 					_self.showList = false
 				}, 50)
+			}
+		}
+	})
+
+	Vue.component('my-input', {
+		template: '<input class="mui-input-numbox" type="number" ref="input" v-bind:value="value" v-on:input="updateValue($event.target.value)">',
+		props: ['value'],
+		methods: {
+			// 不是直接更新值，而是使用此方法来对输入值进行格式化和位数限制
+			updateValue: function(value) {
+				var formattedValue = value
+					// 删除两侧的空格符
+					.trim()
+					// 保留 2 小数位
+					.slice(
+						0,
+						value.indexOf('.') === -1 ?
+						value.length :
+						value.indexOf('.') + 3
+					)
+				// 如果值不统一，手动覆盖以保持一致
+				if(formattedValue !== value) {
+					this.$refs.input.value = formattedValue
+				}
+				// 通过 input 事件发出数值
+				this.$emit('input', Number(formattedValue))
 			}
 		}
 	})
